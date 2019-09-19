b05db2a7343ec9e0ba284936b3f6032ec2455c66
arturjaakman <artur.jaakman@gmail.com>
2019-09-19 18:03:55 +0300

    Updates

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -14,6 +14,28 @@ class PortfolioController < ApplicationController
   def whizz
   end
 
+  def renderer
+  end
+
+  def modular
+  end
+
+  def unity
+  end
+
+  def udk
+  end
+
+  def max7
+  end
+
+  def download_zip
+    send_file(
+    "#{Rails.root}/public/Synth.zip",
+    filename: "Synth.zip"
+    )
+  end
+
   def download_synth
     send_file(
     "#{Rails.root}/public/SynthesizerStandalone.exe",
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
   get 'portfolio/vst'
   get 'portfolio/whizz'
   get 'portfolio/rewinder'
+  get 'portfolio/modular'
+  get 'portfolio/renderer'
+  get 'portfolio/unity'
+  get 'portfolio/udk'
+  get 'portfolio/max7'
   get 'portfolio/download_synth'
+  get 'portfolio/download_zip'
   root 'portfolio#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
